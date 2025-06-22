cfaf72c5f53e706d9fa1a02d68d365c9aaa91492
arvind <akmanutd01@yahoo.in>
2025-06-22 15:06:03 -0500

    vercel analytics

--- src/app/home2/layout.jsx
+++ src/app/home2/layout.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header2 from '../Components/Header/Header2';
 import Footer1 from '../Components/Footer/Footer1';
+import { Analytics } from '@vercel/analytics/next';
 
 const layout = ({ children }) => {
     return (
         <div className='main-page-area bg-color2'>
             <Header2></Header2>
             {children}
+            <Analytics />
             <Footer1></Footer1>
         </div>
     );
